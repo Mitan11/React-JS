9d234ce5422c3f97871777de7d9210786880958c
Mitan <mitantank00@gamil.com>
2024-10-26 11:25:35 +0530

    Bootstrap

---
+++ integrateBootstrapTemplate/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Template from './components/Template';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Template />
+    </>
+  )
+}
+
+export default App
---
+++ integrateBootstrapTemplate/src/components/Template.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Template() {
+    return (
+        <div>
+            <h1 className='fw-bold text-primary'>Hello, world!</h1>
+        </div>
+    )
+}
+
+export default Template
