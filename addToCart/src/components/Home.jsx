609b81b1d04890d3e73e54795dc17466b587ed3f
mitantank.22.imca <mitantank.22.imca@iict.indusuni.ac.in>
2024-12-10 14:54:14 +0000

    addtocart

--- addToCart/src/components/Home.jsx
+++ addToCart/src/components/Home.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import NavBar from './NavBar'
+import Card from './Card'
+
+export const cartcontext = createContext();
+export const addtocartcontext = createContext();
 
 function Home() {
+
+    const [cartCount, setCartCount] = useState(0);
+    const addtocart = () => {
+        setCartCount(cartCount + 1);
+    }
+
     return (
         <>
+            <cartcontext.Provider value={cartCount}>
+                <addtocartcontext.Provider value={addtocart}>
 
-            <NavBar />
-        
+                    <NavBar />
+                    <div className='px-4 md:px-8 flex justify-evenly flex-wrap gap-3'>
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+
+                </addtocartcontext.Provider>
+            </cartcontext.Provider>
+
         </>
     )
 }
--- addToCart/src/components/NavBar.jsx
+++ addToCart/src/components/NavBar.jsx
@@ -1,54 +1,97 @@
-import React from 'react'
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:983741265.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:139162809.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1771084470.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:137291475.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1473495660.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:612749558.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3350450740.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1788955431.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:983402799.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:4141969229.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1662021899.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:856371585.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2998421445.
+import React, { useContext } from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
+import { useState } from 'react';
+import { cartcontext } from './Home';
 
 function NavBar() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const cartCount = useContext(cartcontext);
     return (
         <>
-            <nav className="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
-                <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
-                    {/* Logo */}
-                    <div className="text-indigo-500 md:order-1">
-                        {/* Heroicon - Chip Outline */}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-10 w-10"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
-                            />
-                        </svg>
+            <nav className="bg-gray-200 shadow shadow-gray-300 w-full px-4 py-4 md:px-8">
+                <div className="md:flex items-center justify-between">
+                    <div className="flex justify-between items-center">
+                        <div className="text-indigo-500">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-10 w-10"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+                                />
+                            </svg>
+                        </div>
+
+                        {/* Hamburger button */}
+                        <div className="md:hidden">
+                            <button
+                                onClick={() => setNavbarOpen(!navbarOpen)}
+                                className="text-gray-500 hover:text-gray-700 focus:outline-none"
+                            >
+                                <svg
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    {navbarOpen ? (
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    ) : (
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M4 6h16M4 12h16m-7 6h7"
+                                        />
+                                    )}
+                                </svg>
+                            </button>
+                        </div>
                     </div>
-                    <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
-                        <ul className="flex font-semibold justify-between">
-                            {/* Active Link = text-indigo-500
-          Inactive Link = hover:text-indigo-500 */}
-                            <li className="md:px-4 md:py-2 text-indigo-500">
-                                <a href="#">Dashboard</a>
-                            </li>
-                            <li className="md:px-4 md:py-2 hover:text-indigo-400">
-                                <a href="#">Search</a>
-                            </li>
-                            <li className="md:px-4 md:py-2 hover:text-indigo-400">
-                                <a href="#">Explore</a>
-                            </li>
-                            <li className="md:px-4 md:py-2 hover:text-indigo-400">
-                                <a href="#">About</a>
-                            </li>
-                            <li className="md:px-4 md:py-2 hover:text-indigo-400">
-                                <a href="#">Contact</a>
-                            </li>
+
+                    <div className={`md:flex ${navbarOpen ? 'block' : 'hidden'}`}>
+                        <ul className="md:flex md:items-center font-semibold justify-between space-x-4 md:space-x-6">
+                            {[
+                                { name: 'Dashboard', href: '#', current: true },
+                                { name: 'Search', href: '#', current: false },
+                                { name: 'Explore', href: '#', current: false },
+                                { name: 'About', href: '#', current: false },
+                                { name: 'Contact', href: '#', current: false },
+                            ].map((link) => (
+                                <li key={link.name} className={`py-2 md:my-0 ${link.current ? 'text-indigo-500' : 'hover:text-indigo-400'}`}>
+                                    <a href={link.href}>{link.name}</a>
+                                </li>
+                            ))}
                         </ul>
-                    </div>
-                    <div className="order-2 md:order-3">
-                        <button className="px-4 py-2 flex items-center gap-2">
-                            <FaShoppingCart /><sup>1</sup>
-                        </button>
+                        <div className="md:ml-6 mt-3 md:mt-0">
+                            <button className="px-4 py-2 flex items-center">
+                                <FaShoppingCart /> <sup className="bg-red-500 text-white text-xs rounded-full w-[19px] h-[19px]"><small>{cartCount}</small></sup>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </nav>
